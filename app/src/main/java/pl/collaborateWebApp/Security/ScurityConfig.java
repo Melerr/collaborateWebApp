7879c9be1aa47dde51a30ac27858ecdb0dc5f2d5
Melerr <mestor1@gmail.com>
2020-11-16 17:42:00 +0100

    Add packages Security and Controllers and first implementations

---
+++ app/src/main/java/pl/collaborateWebApp/Security/ScurityConfig.java
@@ -0,0 +1,27 @@
+package pl.collaborateWebApp.Security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class ScurityConfig extends WebSecurityConfigurerAdapter{
+	
+	//Aythntication 
+	// TO_DO
+	
+	
+	// Autorizathion
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.
+			authorizeRequests()
+			.antMatchers("/").permitAll()
+			.anyRequest().authenticated()
+		.and()
+		.formLogin();
+
+	}
+}
